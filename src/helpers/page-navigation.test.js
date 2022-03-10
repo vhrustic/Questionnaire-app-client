9c61cf9db2247e8df05235a758b01522dc5331cc
Vejsil Hrustic <bakerundo@gmail.com>
2022-03-10 09:49:59 +0100

    Github actions for tests

---
+++ src/helpers/page-navigation.test.js
@@ -0,0 +1,8 @@
+import { getPreviousPageId } from "./page-navigation";
+
+describe("Page navigation test", () => {
+  it("getPreviousPageId returns previous page id", () => {
+    const prevPage = getPreviousPageId([{ id: 0 }, { id: 1 }], 1);
+    expect(prevPage).toBe(0);
+  });
+});
